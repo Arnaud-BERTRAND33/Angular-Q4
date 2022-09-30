6790e4fffa52c1eda0f71e5575924c5e9848dd97
Arnaud Bertrand <arnaud.bertrand157@gmail.com>
2022-09-30 11:00:01 +0200

    Quest

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import * as path from "path";
+import {MenuComponent} from "./menu/menu.component";
+import {SignUpComponent} from "./sign-up/sign-up.component";
+import {UserProfileComponent} from "./user-profile/user-profile.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'menu', component: MenuComponent },
+  { path: 'sign-up', component: SignUpComponent },
+  { path: 'user', component: UserProfileComponent },
+  { path: '', component: UserProfileComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
